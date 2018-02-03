0cee759f3c835bdaceb60f83c15866bdb7060841
Phillip <phillipables1@gmail.com>
2018-02-03 01:58:19 -0500

    strict mode require dependencies and set up our express app

---
+++ index.js
@@ -0,0 +1,11 @@
+//we have to do var or let
+//something nice to have
+'use strict'
+
+//write in the fact that we requir our dependencies
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+// initialize app and its going to be an express application
+const app = express()
